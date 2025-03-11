eece1dc40da762b9dd53cdf3485cd5edaf431fa0
EstebanPeña <esteban.coronado0613@gmail.com>
2025-03-10 20:53:25 -0500

    initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/com/maven_smart_logistics/smart_logistics/SmartLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.maven_smart_logistics.smart_logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLogisticsApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/maven_smart_logistics/smart_logistics/SmartLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven_smart_logistics.smart_logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
